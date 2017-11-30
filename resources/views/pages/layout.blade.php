4b388029331a67a7a83291a11c8218f7a41e41e6
erco L <ercoranger@gmail.com>
2017-11-30 15:40:00 +0800

    enhance SEO

--- resources/views/pages/layout.blade.php
+++ resources/views/pages/layout.blade.php
@@ -4,13 +4,18 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="hevo | design factory on metal making">
-    <meta name="keywords" content="metal,design,factory" />
+    <meta name="description" content="非機械大量製造,也不是帶著距離的藝術品.由台灣傳統工廠老職人手工製作的日常生活用品.希望讓數十載製作經驗延續,運用想像力創造鈑金可能.我們選擇賦予熟悉的鐵材嶄新功能,結合多種樣貌.天天能使用且人人可擁有,在日常中發掘鐵之優美.">
+    <meta name="keywords" content="metal,design,factory,金屬設計家具,客製化" />
     <meta name="author" content="erco L., Ricky H.">
+    <meta property="fb:admins" content="896973380414328"/>
+    <meta property="fb:pages" content="896973380414328"/>
     <meta property="og:title" content="hevo | 日何百鐵">
+    <meta property="og:type" content="website">
     <meta property="og:url" content="http://www.hevodeco.com">
     <meta property="og:image" content="http://i.imgur.com/5pL7NHC.jpg">
-    <meta property="og:image:secure_url" content="https://i.imgur.com/5pL7NHC.jpg">
+    <meta property="og:image:secure_url" content="https://i.imgur.com/5pL7NHC.jpg">   
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
     <meta property="og:description" content="非機械大量製造,也不是帶著距離的藝術品.由台灣傳統工廠老職人手工製作的日常生活用品.希望讓數十載製作經驗延續,運用想像力創造鈑金可能.我們選擇賦予熟悉的鐵材嶄新功能,結合多種樣貌.天天能使用且人人可擁有,在日常中發掘鐵之優美.">
 
     <title>@yield('title')</title>
